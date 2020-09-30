370ce66d0c1333975200dda79489b78f41d590a1
cdubles <33189090+cdubles@users.noreply.github.com>
2020-09-29 23:47:51 -0400

    Add to the guides section

--- guides.js
+++ guides.js
@@ -1,14 +1,8 @@
 $(document).ready(function(){
-    
+
 });
 
 function toggle(clicked){
-    clicked = '#'+clicked
-    if($(clicked).css("background-color")=="red"){
-        $(clicked).css("background-color","red")   
-    }
-   else{
-    $(clicked).css("background-color","blue")
-   }
-   console.log($(clicked).css("background-color"))
+    clicked = '#'+clicked;
+    $("div"+clicked).toggle();
 }
